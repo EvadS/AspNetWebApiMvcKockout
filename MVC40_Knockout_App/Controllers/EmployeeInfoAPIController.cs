8d728074a7bdeec97dfe61ab38edd6b616d9fe37
Skiba Evgeniy <Skiba.Evgeniy@gmail.com>
2016-05-06 19:27:43 +0300

    impliment  Crud in api controller

--- MVC40_Knockout_App/Controllers/EmployeeInfoAPIController.cs
+++ MVC40_Knockout_App/Controllers/EmployeeInfoAPIController.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using MVC40_Knockout_App.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -9,31 +12,95 @@ namespace MVC40_Knockout_App.Controllers
 {
     public class EmployeeInfoAPIController : ApiController
     {
-        // GET api/employeeinfoapi
-        public IEnumerable<string> Get()
+        private CompanyEntities db = new CompanyEntities();
+
+        // GET api/EmployeeInfoAPI
+        public IEnumerable<EmployeeInfo> GetEmployeeInfoes()
         {
-            return new string[] { "value1", "value2" };
+            return db.EmployeeInfoes.AsEnumerable();
         }
 
-        // GET api/employeeinfoapi/5
-        public string Get(int id)
+        // GET api/EmployeeInfoAPI/5
+        public EmployeeInfo GetEmployeeInfo(int id)
         {
-            return "value";
+            EmployeeInfo employeeinfo = db.EmployeeInfoes.Find(id);
+            if (employeeinfo == null)
+            {
+                throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.NotFound));
+            }
+
+            return employeeinfo;
         }
 
-        // POST api/employeeinfoapi
-        public void Post([FromBody]string value)
+        // PUT api/EmployeeInfoAPI/5
+        public HttpResponseMessage PutEmployeeInfo(int id, EmployeeInfo employeeinfo)
         {
+            if (ModelState.IsValid && id == employeeinfo.EmpNo)
+            {
+                db.Entry(employeeinfo).State = EntityState.Modified;
+
+                try
+                {
+                    db.SaveChanges();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    return Request.CreateResponse(HttpStatusCode.NotFound);
+                }
+
+                return Request.CreateResponse(HttpStatusCode.OK);
+            }
+            else
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
         }
 
-        // PUT api/employeeinfoapi/5
-        public void Put(int id, [FromBody]string value)
+        // POST api/EmployeeInfoAPI
+        public HttpResponseMessage PostEmployeeInfo(EmployeeInfo employeeinfo)
         {
+            if (ModelState.IsValid)
+            {
+                db.EmployeeInfoes.Add(employeeinfo);
+                db.SaveChanges();
+
+                HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.Created, employeeinfo);
+                response.Headers.Location = new Uri(Url.Link("DefaultApi", new { id = employeeinfo.EmpNo }));
+                return response;
+            }
+            else
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
         }
 
-        // DELETE api/employeeinfoapi/5
-        public void Delete(int id)
+        // DELETE api/EmployeeInfoAPI/5
+        public HttpResponseMessage DeleteEmployeeInfo(int id)
         {
+            EmployeeInfo employeeinfo = db.EmployeeInfoes.Find(id);
+            if (employeeinfo == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound);
+            }
+
+            db.EmployeeInfoes.Remove(employeeinfo);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, employeeinfo);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
         }
     }
 }
