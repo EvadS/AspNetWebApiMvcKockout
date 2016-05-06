4c21388f7992940f2acb45a0fda39e8daafa8497
Skiba Evgeniy <Skiba.Evgeniy@gmail.com>
2016-05-06 19:04:04 +0300

    add EF:entity / model/context

---
+++ MVC40_Knockout_App/Models/CompanyEntities.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MVC40_Knockout_App.Models
+{
+    public class CompanyEntities   :DbContext
+    {
+        public DbSet<EmployeeInfo> EmployeeInfoes { get; set; }
+    }
+}
---
+++ MVC40_Knockout_App/Models/EmployeeInfo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC40_Knockout_App.Models
+{
+    public class EmployeeInfo
+    {
+        [Key]
+        public int EmpNo { get; set; }
+
+        [MaxLength(50)]
+        public string EmpName { get; set; }
+
+        public decimal Salary { get; set; }
+
+        [MaxLength(50)]
+        public string DeptName { get; set; }
+
+        [MaxLength(50)]
+        public string Designation { get; set; }
+    }
+
+}
